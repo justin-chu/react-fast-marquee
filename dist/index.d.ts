804c519e06d19bb1a1556d687862f4fbd4684cae
justin-chu <justinchu252@gmail.com>
2020-10-16 01:52:08 -0400

    Remove dist

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import './styles.scss';
-interface ISmoothCarouselProps extends React.PropsWithChildren<any> {
-    speed?: number;
-    style?: React.CSSProperties;
-}
-declare const SmoothCarousel: React.FC<ISmoothCarouselProps>;
-export default SmoothCarousel;
